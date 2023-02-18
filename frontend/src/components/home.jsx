5a31c9bf09066f263fb6efbaee64acb076f6c36c
zwe <zwezeya02@gmail.com>
2023-02-18 17:32:26 +0800

    add home pg styles

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
     
     return(
         <div>
-            <h1>Are you ready to find your&nbsp;
+            <h1 style={{color: "#FDEFF4"}} >Are you ready to find your&nbsp;
                 <span className="magic">
                     <span className="magic-star"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span>
                     <span className="magic-star"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span>
@@ -42,8 +42,8 @@ const Home = () => {
                 </span>
             </h1>
             <div className="buttons">
-            <button onClick={()=>navigate('/login')}>Log in</button>
-            <button onClick={()=>navigate('/signup')}>Sign up</button>
+            <button style={{backgroundColor: "#5CB8E4", color: "1A0000"}} onClick={()=>navigate('/login')}>Log in</button>
+            <button style={{backgroundColor: "#7A0BC0", color: "#FDEFF4"}} onClick={()=>navigate('/signup')}>Sign up</button>
             </div>
         </div>
     );
